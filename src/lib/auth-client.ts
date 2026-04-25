2dee6d945f196b6c2f0d936eb1b700b3de1a246f
Ishtiaq Robin <robin.pass36@gmail.com>
2026-04-26 00:19:13 +0600

    update auth-client file

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,6 +4,9 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_AUTH_URL,
+  fetchOptions: {
+    credentials: "include", // এটি অত্যন্ত গুরুত্বপূর্ণ ক্রস-ডোমেইন কুকির জন্য
+  },
   plugins: [emailOTPClient()],
 });
 

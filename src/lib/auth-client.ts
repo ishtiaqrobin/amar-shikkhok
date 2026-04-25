a9e77ca32886d53d1adab617148949e87f34e613
Ishtiaq Robin <robin.pass36@gmail.com>
2026-04-25 23:07:07 +0600

    update frontend

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,14 +1,10 @@
-import {
-  emailOTPClient,
-  inferAdditionalFields,
-} from "better-auth/client/plugins";
-import type { Auth } from "../../../Backend/src/app/lib/auth";
+import { emailOTPClient } from "better-auth/client/plugins";
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_AUTH_URL,
-  plugins: [emailOTPClient(), inferAdditionalFields<Auth>()],
+  plugins: [emailOTPClient()],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;

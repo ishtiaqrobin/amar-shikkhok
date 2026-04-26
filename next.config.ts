e9126cbb45a37198386c4ff660cc07b6554ac60e
Ishtiaq Robin <robin.pass36@gmail.com>
2026-04-26 09:17:52 +0600

    use lenis

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
+import LenisProvider from "@/providers/LenisProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +36,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <LenisProvider>{children}</LenisProvider>
         </ThemeProvider>
         <Toaster />
       </body>
